31dab21e273d40d1facb8b6cee2224f18bf4d53d
sebapersson <sebastian.persson1@gmail.com>
2019-12-19 17:23:32 +0100

    Add print-statements

--- Code/Python/RD_models/Run_RD_simulations.py
+++ Code/Python/RD_models/Run_RD_simulations.py
@@ -5,6 +5,7 @@ from Simulate_RD_models import *
 # Check for optimal mesh-size 
 # ------------------------------------------------------------------------------
 # Find mesh size Schankenberg
+print("Checking for optimal mesh-size")
 lc_list = ["0.1", "0.06", "0.08"];
 n_holes_list = ["Zero_holes", "Five_holes", "Twenty_holes"]
 t_opt = t_opt(7.5, 1500)
@@ -21,6 +22,7 @@ find_mesh_size(lc_list, n_holes_list, "Gierer", "Circles", n_time_step, t_end, p
 # ------------------------------------------------------------------------------
 # Running simulations without a specific disturbance in the steady state
 # ------------------------------------------------------------------------------
+print("Solving without controlled disturbance")
 # Schankenberg
 param = param_schankenberg(gamma=10, d=100)
 t_opt = t_opt(7.5, 2000)
@@ -36,6 +38,7 @@ run_rd_sim(param, t_opt, model="Gierer", geom="Circles", times_run=40)
 # ------------------------------------------------------------------------------
 # Running simulations with a controlled disturbance steady state 
 # ------------------------------------------------------------------------------
+print("Running with controlled initial disturbance")
 # The initial controlled conditions 
 ic_par_zero = init_val_param(True, 0, 0, 1.0)
 ic_par_five = init_val_param(True, -0.15, 0.5, 0.25)
