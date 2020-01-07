5e41fa547be9685c633562995593f2cab2ca54c4
sebapersson <sebastian.persson1@gmail.com>
2020-01-07 09:38:57 +0100

    Add working version

--- Code/Python/RD_models/Run_RD_simulations.py
+++ Code/Python/RD_models/Run_RD_simulations.py
@@ -7,8 +7,9 @@ from Simulate_RD_models import *
 # ==================================================================================
 # Find mesh size Schankenberg
 print("Checking for optimal mesh-size")
-lc_list = ["0.1", "0.06", "0.08"];
+lc_list = ["0.03", "0.05", "0.04"];
 n_holes_list = ["Zero_holes", "Five_holes", "Twenty_holes"]
+n_holes_list = ["Twenty_holes"]
 t_opt = t_opt_class(7.5, 1500)
 param = param_schankenberg(gamma=10, d=100)
 #find_mesh_size(lc_list, n_holes_list, "Schankenberg", "Rectangles", t_opt, param)
@@ -26,17 +27,18 @@ param = param_gierer(b = 2.0, a = 0.5, gamma = 20, d = 50)
 # ==================================================================================
 print("Running without controlled disturbance")
 hl = ["Zero_holes", "Five_holes", "Twenty_holes"]
+hl = ["Seven_holes"]
 # Schankenberg
 param = param_schankenberg(gamma=10, d=100)
 t_opt = t_opt_class(7.5, 2000)
-#run_rd_sim(param, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, times_run=10)
-#run_rd_sim(param, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, times_run=10)
+run_rd_sim(param, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, times_run=5)
+run_rd_sim(param, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, times_run=5)
 
 # Gierer
 param = param_gierer(b = 2.0, a = 0.5, gamma = 20, d = 50)
 t_opt = t_opt_class(1.5, 2000)
-#run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, times_run=10)
-#run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, times_run=10)
+run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, times_run=5)
+run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, times_run=5)
 
 
 # ==================================================================================
@@ -52,14 +54,14 @@ ic_list = [ic_par_zero, ic_par_five, ic_par_twenty]
 # Schankenberg
 param = param_schankenberg(gamma=10, d=100)
 t_opt = t_opt_class(7.5, 2000)
-#run_rd_sim(param, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, ic_list=ic_list, times_run=10)
-#run_rd_sim(param, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, ic_list=ic_list, times_run=10)
+run_rd_sim(param, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, ic_list=ic_list, times_run=5)
+run_rd_sim(param, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, ic_list=ic_list, times_run=5)
 
 # Gierer
 param = param_gierer(b = 2.0, a = 0.5, gamma = 20, d = 50)
 t_opt = t_opt_class(1.5, 2000)
-#run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, ic_list=ic_list, times_run=10)
-#run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, ic_list=ic_list, times_run=10)
+run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, ic_list=ic_list, times_run=5)
+run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, ic_list=ic_list, times_run=5)
 
 
 # ==================================================================================
@@ -74,16 +76,16 @@ t_opt = t_opt_class(7.5, 2000)
 # Increasing production 
 diff_par = [diff_param_class([2], param_schankenberg(a=0.5, b=2.0, d=100, gamma=10)),
             diff_param_class([15], param_schankenberg(a=0.5, b=2.0, d=100, gamma=10))]
-#run_rd_sim(param, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=10)
-#run_rd_sim(param, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=10)
+run_rd_sim(param, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=5)
+run_rd_sim(param, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=5)
 
 # Gierer
 param = param_gierer(b = 2.0, a = 0.5, gamma = 20, d = 50)
 t_opt = t_opt_class(1.5, 2000)
 diff_par = [diff_param_class([2], param_gierer(a=0.6, b=2.0, d=50, gamma=20)),
             diff_param_class([15], param_gierer(a=0.6, b=2.0, d=50, gamma=20))]
-#run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=10)
-#run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=10)
+run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=5)
+run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=5)
 
 ''' 
 As the holes end up in a controlled manner for twenty holes this will focus on, 
@@ -97,15 +99,15 @@ param = param_schankenberg(gamma=10, d=200)
 t_opt = t_opt_class(7.5, 2000)
 # Increasing production 
 diff_par = [diff_param_class([2], param_schankenberg(a=0.6, b=2.0, d=200, gamma=10))]
-#run_rd_sim(param, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=10)
-#run_rd_sim(param, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=10)
+run_rd_sim(param, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=5)
+run_rd_sim(param, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=5)
 
 # Gierer 
 param = param_gierer(b = 2.0, a = 0.5, gamma = 20, d = 100)
 t_opt = t_opt_class(1.5, 2000)
 diff_par = [diff_param_class([2], param_gierer(a=0.65, b=2.0, d=100, gamma=20))]
-#run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=10)
-#run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=10)
+run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=5)
+run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=5)
 
 
 ## Heavily increase production (a parameter)
@@ -114,15 +116,15 @@ param = param_schankenberg(gamma=10, d=100)
 t_opt = t_opt_class(7.5, 2000)
 # Increasing production 
 diff_par = [diff_param_class([2], param_schankenberg(a=2.0, b=2.0, d=100, gamma=10))]
-#run_rd_sim(param, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=10)
-#run_rd_sim(param, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=10)
+run_rd_sim(param, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=5)
+run_rd_sim(param, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=5)
 
 # Gierer 
 param = param_gierer(b = 2.0, a = 0.5, gamma = 20, d = 50)
 t_opt = t_opt_class(1.5, 2000)
 diff_par = [diff_param_class([2], param_gierer(a=2.0, b=2.0, d=50, gamma=20))]
-#run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=10)
-#run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=10)
+run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=5)
+run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=5)
 
 ## Decrease the breakdown rate and heavily increase the production, for the Schankenberg model a controlled pattern
 ## is formed if the parameters are heavily controlled 
@@ -132,17 +134,23 @@ param2 = param_schankenberg(gamma=10, d=800)
 t_opt = t_opt_class(1.0, 3000)
 # Increasing production 
 diff_par = [diff_param_class([2], param_schankenberg(a=2.5, b=0.2, d=100, gamma=10))]
-#run_rd_sim(param1, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=10)
-#run_rd_sim(param2, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=10)
-#run_rd_sim(param1, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=10)
-#run_rd_sim(param2, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=10)
+run_rd_sim(param1, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=5)
+run_rd_sim(param2, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=5)
+run_rd_sim(param1, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=5)
+run_rd_sim(param2, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=5)
 
 # Gierer 
 param = param_gierer(b = 2.0, a = 0.5, gamma = 20, d = 50)
 t_opt = t_opt_class(1.5, 2000)
 diff_par = [diff_param_class([2], param_gierer(a=2.0, b=0.5, d=50, gamma=20))]
-#run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=10)
-#run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=10)
+run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=5)
+run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=5)
+
+# For both cases check what happens with Gierer when decreasing d
+param_list = [param_gierer(b = 2.0, a = 0.4, gamma = 20, d = 40), param_gierer(b = 2.0, a = 0.4, gamma = 20, d = 25)]
+for param in param_list:
+    run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=5)
+    run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=5)
 
 
 ## As a larger a and smaller b produces good result, check seven-holes case
@@ -152,15 +160,15 @@ hl = ["Seven_holes"]
 param = param_schankenberg(gamma=10, d=800)
 t_opt = t_opt_class(1.0, 3000)
 diff_par = [diff_param_class([7], param_schankenberg(a=2.5, b=0.2, d=100, gamma=10))]
-run_rd_sim(param1, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=10)
-run_rd_sim(param1, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=10)
+run_rd_sim(param1, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=5)
+run_rd_sim(param1, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=5)
 
 # Gierer
 param = param_gierer(b = 2.0, a = 0.5, gamma = 20, d = 50)
 t_opt = t_opt_class(1.5, 2000)
 diff_par = [diff_param_class([2], param_gierer(a=2.0, b=0.5, d=50, gamma=20))]
-run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=10)
-run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=10)
+run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, diff_par_list=diff_par, times_run=5)
+run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, diff_par_list=diff_par, times_run=5)
 
 # ==================================================================================
 # Control both initial steady state and the parameters 
@@ -178,16 +186,16 @@ param = param_schankenberg(gamma=20, d=50)
 t_opt = t_opt_class(10.0, 2000)
 diff_par = [diff_param_class([2], param_schankenberg(a=0.3, b=2.0, d=50, gamma=20)),
             diff_param_class([15], param_schankenberg(a=0.3, b=2.0, d=50, gamma=20))]
-#run_rd_sim(param, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, ic_list=ic_list, diff_par_list=diff_par, times_run=10)
-#run_rd_sim(param, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, ic_list=ic_list, diff_par_list=diff_par, times_run=10)
+run_rd_sim(param, t_opt, model="Schankenberg", geom="Circles", hole_list=hl, ic_list=ic_list, diff_par_list=diff_par, times_run=5)
+run_rd_sim(param, t_opt, model="Schankenberg", geom="Rectangles", hole_list=hl, ic_list=ic_list, diff_par_list=diff_par, times_run=5)
 
 # Gierer
 param = param_gierer(b = 2.0, a = 0.5, gamma = 20, d = 50)
 t_opt = t_opt_class(1.5, 2000)
 diff_par = [diff_param_class([2], param_gierer(a=0.6, b=2.0, d=50, gamma=20)),
             diff_param_class([15], param_gierer(a=0.6, b=2.0, d=50, gamma=20))]
-#run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, ic_list=ic_list, diff_par_list=diff_par, times_run=10)
-#run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, ic_list=ic_list, diff_par_list=diff_par, times_run=10)
+run_rd_sim(param, t_opt, model="Gierer", geom="Rectangles", hole_list=hl, ic_list=ic_list, diff_par_list=diff_par, times_run=5)
+run_rd_sim(param, t_opt, model="Gierer", geom="Circles", hole_list=hl, ic_list=ic_list, diff_par_list=diff_par, times_run=5)
 
 # ==================================================================================
 # Sanity check the solutions
